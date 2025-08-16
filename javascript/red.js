a76fcda09ea2f1ad7ace9800b6fe71c0972543d4
Ruban5166 <49543080+Ruban5166@users.noreply.github.com>
2025-08-16 17:05:45 +0530

    Add files via upload

---
+++ javascript/red.js
@@ -0,0 +1,25 @@
+// const numbers = [1,2,3,4,5,6,7,8,9,10]
+
+
+
+// const sumOfevn = numbers.reduce((sum,num)=>{
+//     if(num%2 == 0){
+//         return sum = sum + num
+//     }
+
+//     return sum;
+// },0)
+
+
+
+// console.log(sumOfevn);
+
+
+
+function replacethevow(str){
+    return str.replace(/[aeiouAEIOU]/g,'')
+}
+
+
+console.log(replacethevow('srikishantharuban'));
+
